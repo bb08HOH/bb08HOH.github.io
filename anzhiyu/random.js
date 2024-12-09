45517dcd5d7912ce25bbb365086d5ec7553d8234
bb08HOH <2821556998@qq.com>
2024-12-09 19:05:10 +0800

    Site updated: 2024-12-09 19:05:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/03/hello-world/","2024/12/09/一些基础的代码题目以及题解分享/"];function toRandomPost(){
+var posts=["2024/12/09/1-4章/","2024/12/03/hello-world/","2024/12/09/一些基础的代码题目以及题解分享/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
